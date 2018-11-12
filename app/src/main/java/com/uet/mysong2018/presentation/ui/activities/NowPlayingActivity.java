27cb9aa4eced4c204cf72673b6c65e78f96a8651
SyPT <sy.phamtiennd>
2018-11-13 00:55:55 +0700

    SyPT

--- app/src/main/java/com/uet/mysong2018/presentation/ui/activities/NowPlayingActivity.java
+++ app/src/main/java/com/uet/mysong2018/presentation/ui/activities/NowPlayingActivity.java
@@ -3,7 +3,6 @@ package com.uet.mysong2018.presentation.ui.activities;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
-import android.support.v4.content.res.ResourcesCompat;
 import android.view.MenuItem;
 
 import com.uet.mysong2018.BaseActivity;
